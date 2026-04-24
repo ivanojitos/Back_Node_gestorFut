c072561775cdd8cb0dd25b926d380f3a7b810c83
Ivan Alvarez Valencia (el pari) <ivan.alvarez@servimsa.com.mx>
2026-04-24 08:09:45 -0600

    se configuro para node v2

--- src/config/db.js
+++ src/config/db.js
@@ -1,24 +1,26 @@
 const sql = require('mssql');
 require('dotenv').config();
 
-const config = {
-    user: process.env.DB_USER,
+const dbConfig = {
+    user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    server: process.env.DB_SERVER,
+    server: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
-    port: 1433,
+    port: parseInt(process.env.DB_PORT),
     options: {
         encrypt: true,
         trustServerCertificate: true
     }
 };
 
-const poolPromise = new sql.ConnectionPool(config)
-    .connect()
-    .then(pool => {
-        console.log("✔ SQL Server conectado");
-        return pool;
-    })
-    .catch(err => console.log("❌ DB Error", err));
+let pool;
 
-module.exports = { sql, poolPromise };
+const getPool = async () => {
+    if (!pool) {
+        pool = await sql.connect(dbConfig);
+        console.log("SQL Connected");
+    }
+    return pool;
+};
+
+module.exports = { sql, getPool };
