67c3b334e156f71ec5ff1c8bbb6d174fb9603a58
Ivan Alvarez Valencia (el pari) <ivan.alvarez@servimsa.com.mx>
2026-04-27 09:46:47 -0600

    seguimos con arbitros v2

--- src/controllers/arbitroController.js
+++ src/controllers/arbitroController.js
@@ -31,7 +31,7 @@ exports.getArbitro = async (req, res) => {
   try {
     const { id } = req.params;
 
-    const arbitro = await Arbitro.findByPk(id); // ✅ CORRECTO
+    const arbitro = await Arbitro.findById(id); // ✅ CORRECTO
 
     if (!arbitro) {
       return res.status(404).json({ error: "Árbitro no encontrado" });
