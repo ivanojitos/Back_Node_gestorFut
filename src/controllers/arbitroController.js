0c0fce8c9b8f2585e545359269946027e219c8fa
Ivan Alvarez Valencia (el pari) <ivan.alvarez@servimsa.com.mx>
2026-04-27 10:50:41 -0600

    estamos debuggeando

--- src/controllers/arbitroController.js
+++ src/controllers/arbitroController.js
@@ -10,19 +10,20 @@ exports.updateArbitro = async (req, res) => {
       return res.status(404).json({
         ok: false,
         error: "Árbitro no encontrado",
+        data: req.body,
       });
     }
 
-    res.json({
+    return res.json({
       ok: true,
       message: "Árbitro actualizado",
       data: arbitroActualizado,
     });
-
   } catch (err) {
-    res.status(500).json({
+    return res.status(500).json({
       ok: false,
       error: err.message,
+      data: req.body,
     });
   }
 };
