816da34e7647fa56f619e6ef811cd6f0ec22ddb5
Ivan Alvarez Valencia (el pari) <ivan.alvarez@servimsa.com.mx>
2026-04-27 20:26:06 -0600

    multer instalado v2

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const upload = require("../uploads/");
+const upload = require("../uploads/upload");
 
 const loginController = require('../controllers/loginController');
 const arbitroController = require('../controllers/arbitroController');
