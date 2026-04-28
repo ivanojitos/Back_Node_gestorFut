63685e732dce99215e33e2525f6e46ae8afeaa1f
Ivan Alvarez Valencia (el pari) <ivan.alvarez@servimsa.com.mx>
2026-04-28 10:57:06 -0600

    all ligas get v2

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -21,7 +21,7 @@ router.put('/arbitro/:id', arbitroController.updateArbitro);
 // ✅ CREAR LIGA
 
 // ✅ OBTENER TODAS
-router.get('/ligasGet', ligasController.getLigas);
+router.get('/ligas', ligasController.getLigas);
 
 // ✅ OBTENER UNA
 router.get('/ligas/:id', ligasController.getLigaById);
