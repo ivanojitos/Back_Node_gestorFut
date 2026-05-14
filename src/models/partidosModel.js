a5d853e3faeb6c63d090c8532325b3750fa32163
Ivan Alvarez Valencia (el pari) <ivan.alvarez@servimsa.com.mx>
2026-05-14 13:00:10 -0600

    amejorando la vista de jugadopr con equipo partidos v3

--- src/models/partidosModel.js
+++ src/models/partidosModel.js
@@ -523,9 +523,11 @@ const getPartidosEquipoPaginado = async (idEquipo, page = 1, limit = 10) => {
       rj.Id_Cancha,
       rj.Goles_Local,
       rj.Goles_Visitante,
-
+      el.Logo,
+      
       el.Nombre AS local_nombre,
-      ev.Nombre AS visitante_nombre
+      ev.Nombre AS visitante_nombre,
+      
 
     FROM Rol_Juego rj
 
